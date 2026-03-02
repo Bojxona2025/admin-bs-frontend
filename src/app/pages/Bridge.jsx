b6740454c838a736a21f5071915a98b20b2b1122
Your Name <you@example.com>
2026-03-02 13:54:08 +0500

    tugirlandi

--- src/app/pages/Bridge.jsx
+++ src/app/pages/Bridge.jsx
@@ -1,31 +1,31 @@
-import { useEffect } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+// import { useEffect } from "react";
+// import { useNavigate, useSearchParams } from "react-router-dom";
 
-export default function Bridge() {
-  const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
+// export default function Bridge() {
+//   const [searchParams] = useSearchParams();
+//   const navigate = useNavigate();
 
-  useEffect(() => {
-    const token = searchParams.get("token");
+//   useEffect(() => {
+//     const token = searchParams.get("token");
 
-    if (!token) {
-      navigate("/login"); // yoki o'zingni login page
-      return;
-    }
+//     if (!token) {
+//       navigate("/login"); // yoki o'zingni login page
+//       return;
+//     }
 
-    // Tokenni admin panel localStorage'iga yozamiz
-    localStorage.setItem("admin_token", token);
-    localStorage.setItem("accessToken", token);
+//     // Tokenni admin panel localStorage'iga yozamiz
+//     localStorage.setItem("admin_token", token);
+//     localStorage.setItem("accessToken", token);
 
-    // URL'dan tokenni olib tashlaymiz (historyda qolmasin)
-    window.history.replaceState({}, document.title, "/indicators/general");
+//     // URL'dan tokenni olib tashlaymiz (historyda qolmasin)
+//     window.history.replaceState({}, document.title, "/indicators/general");
 
-    navigate("/indicators/general");
-  }, [searchParams, navigate]);
+//     navigate("/indicators/general");
+//   }, [searchParams, navigate]);
 
-  return (
-    <div style={{ padding: 20 }}>
-      Admin panelga kirilmoqda...
-    </div>
-  );
-}
+//   return (
+//     <div style={{ padding: 20 }}>
+//       Admin panelga kirilmoqda...
+//     </div>
+//   );
+// }
